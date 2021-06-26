5fb19a1bfbed39bb7e03458420e2b0b7fbcdf672
matudani <daniel.matu@mail.utoronto.ca>
2021-06-26 17:36:44 -0400

    Revert "Revert "added background video""
    This reverts commit ab10d6ef9826b9bdeae1771028be17cbe241a4dc.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,6 +4,7 @@ import createHistory from 'history/createBrowserHistory'
 import DashboardPage from '../components/DashboardPage'
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage'
+import BackgroundVideo from '../components/BackgroundVideo'
 import PrivateRoute from './PrivateRoute'
 import PublicRoute from './PublicRoute'
 
@@ -12,9 +13,14 @@ export const history = createHistory()
 const AppRouter = () => (
     <Router history={history}>
         <div>
+            <BackgroundVideo url='images/bg-sea.mp4'/>
             <Switch>
                 <PublicRoute path="/" component={LoginPage} exact={true}/>
+
                 <PrivateRoute path="/dashboard" component={DashboardPage}/>
+                <PrivateRoute path="/journal" component={DashboardPage}/>
+                <PrivateRoute path="/fortunes" component={DashboardPage}/>
+
                 <Route component= {NotFoundPage} />
             </Switch>
         </div>
